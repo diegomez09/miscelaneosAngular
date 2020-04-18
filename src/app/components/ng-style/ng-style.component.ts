9d9652f3a8dc079a7e5baafc2e751709584e7916
Diego Gomez <diegofabricioggmail.com>
2020-04-18 16:45:09 -0500

    Clase 140

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p>
+      ng-style works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgStyleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
